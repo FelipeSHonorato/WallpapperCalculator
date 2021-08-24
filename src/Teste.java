3483e2416c8e4d32e0c5f086ae219df1c44f6f41
Felipe Silva Honorato <felipeshonorato@gmail.com>
2021-08-24 11:20:31 -0300

    Remoção de retorno de valores

--- src/Teste.java
+++ src/Teste.java
@@ -1,12 +1,10 @@
 /**
+ * Programa de código aberto para cálculo de quantidade de papel de parede a ser utilizada em uma parede
  *
- *  Programa de código aberto para cálculo de quantidade de papel de parede a ser utilizada em uma parede
- *
- * @version 1.0
  * @author Felipe Silva Honorato
  *
  * Testando modificador 1 utilizando Intellij
- *
+ * @version 1.5
  */
 
 public class Teste {
@@ -16,13 +14,8 @@ public class Teste {
 
         info.paredesInformacoes();
         info.papelInformacoes();
-
-        System.out.println(info.getSomaMtQuadPapel());
-        System.out.println(info.getSomaMtQuadParedes());
-
         info.infoCalculoFinal();
+        info.infoQtdCola();
 
-        System.out.println(info.getSomaMtQuadPapel());
-        System.out.println(info.getSomaMtQuadParedes());
     }
 }
